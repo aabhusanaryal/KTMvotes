a47f2f9a08d5125c91e333ac9c7e4c554f6c99e5
Aabhusan Aryal <aabhusanaryal@gmail.com>
2022-05-16 14:12:09 +0545

    Now displays 'Total Votes'

--- index.js
+++ index.js
@@ -2,17 +2,11 @@ require("dotenv").config();
 const Twitter = require("twitter");
 const axios = require("axios");
 const cheerio = require("cheerio");
-const pretty = require("pretty");
 const { Webhook, MessageBuilder } = require("discord-webhook-node");
 
-const url = process.env.dataURL;
+const url = process.env.dataURL; // data URL
 let webhookURLs = process.env.WEBHOOKS.split(" ");
-const hooks = [];
-
-webhookURLs.forEach((url) => {
-  const hook = new Webhook(url);
-  hooks.push(hook);
-});
+const hooks = []; // array of Webhook objects
 
 const client = new Twitter({
   consumer_key: process.env.CONSUMER_KEY,
@@ -21,50 +15,64 @@ const client = new Twitter({
   access_token_secret: process.env.ACCESS_TOKEN_SECRET,
 });
 
-let resultOld = [{ candidateName: "", candidateVote: 0 }];
-let resultNew = [{ candidateName: "", candidateVote: 0 }];
+webhookURLs.forEach((url) => {
+  const hook = new Webhook(url);
+  hooks.push(hook);
+});
+
+let totalCandidates = 20; // currently, we're fetching the data of top 20 candidates only
+let topCandidates = 3; // the number of candidates to display in tweet/ bot msg
+let totalVotes = 0; // total votes secured by `totalCandidates`
+let topVotes = 0; // total votes secured by `topCandidates`
+
+let resultOld = [{ name: "", votes: 0 }];
+let resultNew = [{ name: "", votes: 0 }];
 const interval = 0.1; // in minutes
+
 // Fetching data
 setInterval(() => {
+  totalVotes = 0;
+  topVotes = 0;
   webhookURLs = process.env.WEBHOOKS.split(" ");
-  resultOld = [...resultNew]; // Moving the previous result to resultOld
+  // Moving the previous vote count to resultOld and adding new voute count in resultNew
+  resultOld = [...resultNew];
+
   async function scrapeData() {
     try {
-      // Fetch HTML of the page we want to scrape
+      // Fetch HTML of the page
       const { data } = await axios.get(url);
       // Load HTML we fetched in the previous line
       const $ = cheerio.load(data);
-      // Select all the list items in plainlist class
+      // Select all the list items in .candidate-list
       const listItems = $(".candidate-list");
-
-      // Use .each method to loop through the li we selected
+      // Looping through the list items
       listItems.each((idx, el) => {
-        if (idx <= 3) {
-          // Object holding data for each country/jurisdiction
+        if (idx <= totalCandidates) {
           const candidate = { name: "", votes: "" };
-          // Select the text content of a and span elements
-          // Store the textcontent in the above object
           let candidateMeta = $(el)
             .children(".row")
             .children(".col")
             .children(".candidate-meta-wrapper");
-          let candidateName = candidateMeta
+          let name = candidateMeta
             .children(".candidate-meta")
             .children(".candidate-name")
             .text();
-          let candidateVote = candidateMeta.children(".vote-numbers").text();
-          resultNew[idx] = { candidateName, candidateVote };
+          let votes =
+            parseInt(candidateMeta.children(".vote-numbers").text()) | 0;
+          // The site doesn't have vote count for people that haven't received any votes yet.
+          // The |0 in the previous line converts those NaNs to 0
+          totalVotes += votes;
+          if (idx < topCandidates) topVotes += votes;
+          resultNew[idx] = { name, votes };
         }
       });
 
       // Now, if the entries of resultOld and resultNew differ, it means we have an update
       // In that case, we push out a tweet and trigger webhook
-
-      if (resultOld[0].candidateVote != resultNew[0].candidateVote) {
+      if (resultOld[0].votes != resultNew[0].votes) {
         tweet();
         webhook();
       }
-      // Logs countries array to the console
     } catch (err) {
       console.error(err);
     }
@@ -76,14 +84,14 @@ setInterval(() => {
 function tweet() {
   let status =
     "------------------------------\nKTM Metro Vote Count:\n------------------------------\n\n";
-  resultNew.forEach((candidate) => {
-    status += `${candidate.candidateName}: ${candidate.candidateVote}\n`;
+  resultNew.slice(0, topCandidates).forEach((candidate) => {
+    status += `${candidate.name}: ${candidate.votes.toLocaleString()}\n`;
   });
+  status += `Other: ${(totalVotes - topVotes).toLocaleString()}\n\n`;
+  status += `Total Votes: ${totalVotes.toLocaleString()}\n`;
   status += "\n#LocalElections2022 #LocalElections2079";
   client.post("statuses/update", { status }, function (error, tweet, response) {
     if (error) throw error;
-    console.log(tweet); // Tweet body.
-    console.log(response); // Raw response object.
   });
 }
 
@@ -94,17 +102,15 @@ function webhook() {
       "https://media.discordapp.net/attachments/974998328104337438/975001919158382612/pexels-element-digital-1550337.jpg?width=1023&height=682",
       "https://election.ekantipur.com/pradesh-3/district-kathmandu/kathmandu?lng=eng"
     )
-    // .setURL(`${url}${latestNotice.link}`)
     .setColor("#42c700")
-    //   .setThumbnail("https://cdn.discordapp.com/embed/avatars/0.png")
-    // .setDescription(`${latestNotice.title}`)
-    //   .setImage("https://cdn.discordapp.com/embed/avatars/0.png")
     .setFooter("Bot maintained by Aabhusan Aryal")
     .setTimestamp();
 
-  resultNew.forEach((candidate) => {
-    embed.addField(candidate.candidateName, candidate.candidateVote);
+  resultNew.slice(0, topCandidates).forEach((candidate) => {
+    embed.addField(candidate.name, candidate.votes.toLocaleString());
   });
+  embed.addField("Other", (totalVotes - topVotes).toLocaleString(), true);
+  embed.addField("Total Votes", totalVotes.toLocaleString(), true);
   hooks.forEach((hook) => {
     hook.send(embed);
   });
