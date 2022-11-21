7efef8ede7086ea00ed5c5f99974d0c0cb12d395
Aabhusan Aryal <aabhusanaryal@gmail.com>
2022-11-21 12:51:45 +0545

    Can fetch data from any district-constituency

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const { Webhook, MessageBuilder } = require("discord-webhook-node");
 
-const url = process.env.dataURL; // data URL
 let webhookURLs = process.env.WEBHOOKS.split(" ");
 const hooks = []; // array of Webhook objects
 
@@ -25,80 +24,108 @@ let topCandidates = 3; // the number of candidates to display in tweet/ bot msg
 let totalVotes = 0; // total votes secured by `totalCandidates`
 let topVotes = 0; // total votes secured by `topCandidates`
 
-let resultOld = [{ name: "", votes: 0 }];
-let resultNew = [{ name: "", votes: 0 }];
+let resultOld = [];
+let resultNew = [];
+// Elements:
+// 0:
 const interval = 1; // in minutes
 
 // Fetching data
-setInterval(() => {
-  totalVotes = 0;
-  topVotes = 0;
-  // Moving the previous vote count to resultOld and adding new voute count in resultNew
-  resultOld = [...resultNew];
+// setInterval(() => {
+totalVotes = 0;
+topVotes = 0;
+// Moving the previous vote count to resultOld and adding new voute count in resultNew
+resultOld = [...resultNew];
 
-  async function scrapeData() {
-    try {
-      // Fetch HTML of the page
-      const { data } = await axios.get(url);
-      // Load HTML we fetched in the previous line
-      const $ = cheerio.load(data);
-      // Select all the list items in .candidate-list
-      const listItems = $(".candidate-list");
-      // Looping through the list items
-      listItems.each((idx, el) => {
-        if (idx <= totalCandidates) {
-          let candidate = { name: "", votes: "" };
-          let candidateMeta = $(el)
-            .children(".row")
-            .children(".col")
-            .children(".candidate-meta-wrapper");
-          let name = candidateMeta
-            .children(".candidate-meta")
-            .children(".candidate-name")
-            .text();
-          let votes =
-            parseInt(candidateMeta.children(".vote-numbers").text()) | 0;
-          // The site doesn't have vote count for people that haven't received any votes yet.
-          // The |0 in the previous line converts those NaNs to 0
-          totalVotes += votes;
-          if (idx < topCandidates) topVotes += votes;
-          resultNew[idx] = { name, votes };
-        }
-      });
+async function scrapeData(url, constituency) {
+  result = [];
+  try {
+    // Fetch HTML of the page
+    const { data } = await axios.get(url);
+    // Load HTML we fetched in the previous line
+    const $ = cheerio.load(data);
+    const listItems = $(
+      `.col-md-6:nth-child(${constituency}) .candidate-list__item`
+    );
+    // Looping through the list items
+    listItems.each((idx, el) => {
+      if (idx <= totalCandidates) {
+        let candidateMeta = $(el)
+          .children(".row")
+          .children(".col")
+          .children(".candidate-wrapper");
 
-      // Now, if the entries of resultOld and resultNew differ, it means we have an update
-      // In that case, we push out a tweet and trigger webhook
-      if (resultOld[0].votes != resultNew[0].votes) {
-        tweet();
-        webhook();
+        let name = candidateMeta
+          .children(".candidate-meta")
+          .children(".nominee-name")
+          .text();
+        let votes = parseInt(candidateMeta.children(".vote-count").text());
+        result.push({ name, votes });
       }
-    } catch (err) {
-      console.error(err);
-    }
+    });
+    return result;
+  } catch (err) {
+    console.error(err);
   }
-  // Invoke the above function
-  scrapeData();
-}, interval * (60 * 1000));
+}
 
-function tweet() {
-  let status =
-    "------------------------------\nKTM Metro Vote Count:\n------------------------------\n\n";
+let places = [
+  {
+    district: "Dadeldhura",
+    constituency: "1",
+    url: "https://election.ekantipur.com/pradesh-7/district-dadeldhura?lng=eng",
+  },
+  {
+    district: "Kathmandu",
+    constituency: "4",
+    url: "https://election.ekantipur.com/pradesh-3/district-kathmandu?lng=eng",
+  },
+  {
+    district: "Lalitpur",
+    constituency: "3",
+    url: "https://election.ekantipur.com/pradesh-3/district-lalitpur?lng=eng",
+  },
+];
+
+async function populate() {
+  resultNew = [];
+  for (let i = 0; i < places.length; i++) {
+    let place = places[i];
+    await scrapeData(place.url, place.constituency).then((result) => {
+      resultNew.push({
+        title: `${place.district}-${place.constituency}`,
+        district: place.district,
+        constituency: place.constituency,
+        result: result,
+      });
+    });
+  }
+  return "Done!";
+}
+async function main() {
+  await populate();
+  console.log(resultNew);
+}
+
+main();
+
+function tweet(title) {
+  let status = `------------------------------\n${title} Vote Count:\n------------------------------\n\n`;
   resultNew.slice(0, topCandidates).forEach((candidate) => {
     status += `${candidate.name}: ${candidate.votes.toLocaleString()}\n`;
   });
-  status += `Other: ${(totalVotes - topVotes).toLocaleString()}\n\n`;
-  status += `Total Votes: ${totalVotes.toLocaleString()}\n`;
+
   status += "\n#LocalElections2022 #LocalElections2079";
   client.post("statuses/update", { status }, function (error, tweet, response) {
     if (error) throw error;
   });
 }
 
-function webhook() {
+function webhook(title) {
   const embed = new MessageBuilder()
     .setAuthor(
-      "KTM Metro Vote Count",
-      "https://media.discordapp.net/attachments/974998328104337438/975001919158382612/pexels-element-digital-1550337.jpg?width=1023&height=682",
+      `${title} Vote Count`,
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Election_Commission%2C_Nepal.svg/800px-Election_Commission%2C_Nepal.svg.png",
       url
     )
     .setColor("#42c700")
@@ -108,8 +135,6 @@ function webhook() {
   resultNew.slice(0, topCandidates).forEach((candidate) => {
     embed.addField(candidate.name, candidate.votes.toLocaleString());
   });
-  embed.addField("Other", (totalVotes - topVotes).toLocaleString(), true);
-  embed.addField("Total Votes", totalVotes.toLocaleString(), true);
   hooks.forEach((hook) => {
     hook.send(embed);
   });
